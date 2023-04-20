640009d417af24d72f91b6c8ed53609815747333
RedGaming98 <51047087+viciscat@users.noreply.github.com>
2023-04-20 16:51:38 +0200

    Add ender chest closing detection

--- src/main/java/me/viciscat/mineralcontest/MineralListener.java
+++ src/main/java/me/viciscat/mineralcontest/MineralListener.java
@@ -1,9 +1,14 @@
 package me.viciscat.mineralcontest;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.util.BoundingBox;
 
 public class MineralListener implements Listener {
@@ -16,5 +21,17 @@ public class MineralListener implements Listener {
         if (spawnReason.equals(CreatureSpawnEvent.SpawnReason.NATURAL) && boundingBox.contains(entity.getLocation().toVector())) {
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void onCloseInventory(InventoryCloseEvent event) {
+        if (!(event.getPlayer() instanceof Player player)) return;
+        if (!MineralContest.instance.gameHandlerMap.containsKey(player.getWorld())) return;
+        Inventory inventory = event.getInventory();
+        if (!inventory.getType().equals(InventoryType.ENDER_CHEST)) return;
+
+        player.sendMessage(Component.text("You closed an enderchest! Is it one of the castle's one? Who knows cuz this hasn't been implemented yet!!"));
+
+
     }
 }
