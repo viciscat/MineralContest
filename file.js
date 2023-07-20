e69f0ecc2182e3dc3423c647aa6013e27cffb6d3
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2023-07-21 00:57:25 +0200

    Create file.js

---
+++ file.js
@@ -0,0 +1,9 @@
+function scrollToTop() {
+    document.getElementById("header-nav").scrollIntoView({ behavior: 'smooth' });
+
+}
+
+function scrollToText() {
+    document.getElementById("main_text").scrollIntoView({ behavior: 'smooth' });
+
+}
